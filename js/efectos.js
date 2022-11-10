a8a4ddfb6f4b963839bb8a900a3fc9843d769748
unknown <vichavarria21@gmail.com>
2022-11-10 08:26:45 -0600

    Pagina restaurante

---
+++ js/efectos.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+    /* EFECTO MENU */
+    $('.menu a').each(function(index, elemento){
+        $(this).css({
+            top: '-200px'
+        });
+
+        $(this).animate({
+            top: '0'
+        }, 1000 + (index * 500));
+    });
+
+    /* EFECTO HEADER */
+    if( $(window).width() > 800 ){
+        $('header .textos').css({
+            opacity: '0',
+            marginTop: '0'
+        });
+        $('header .textos').animate({
+            opacity: '1',
+            marginTop: '-52px'
+        }, 1500);
+    };
+
+    /* SCROLL ELEMENTOS MENU */
+    var acercaDE = $('#acerca-de').offset().top,
+        menu = $('#menu').offset().top,
+        galeria = $('#galeria').offset().top,
+        ubicacion = $('#ubicacion').offset().top;
+
+    $('#btn-acerca-de').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: acercaDE - 250
+        }, 500)
+    });
+
+    $('#btn-menu').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: menu - 30
+        }, 500)
+    });
+
+    $('#btn-galeria').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: galeria
+        }, 500)
+    });
+
+    $('#btn-ubicacion').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: ubicacion
+        }, 500)
+    });
+
+});
---
+++ js/parallax.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        var windowWidth =  $(window).width();
+
+        if(windowWidth > 800){
+            var scroll = $(window).scrollTop();
+
+            $('header .textos').css({
+                'transform': 'translate(0px, ' + scroll / 3 + '%)'
+            });
+            $('.acerca-de article').css({
+                'transform': 'translate(0px, -' + scroll / 4 + '%)'
+            });
+        }
+    });
+
+    $(window).resize(function(){
+        var windowWidth = $(window).width();
+        if(windowWidth > 800){
+            $('.acerca-de article').css({
+                'transform': 'translate(0px, 0px)'
+            });
+        }
+    });
+
+});
